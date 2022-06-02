80b55d5333721e05016ce7632bf551dc365381e7
Viet Anh Dang <vietanh.dang282@gmail.com>
2022-06-02 14:15:57 +0700

    Delete all

---
+++ testSwiftPackage/testSwiftPackage/testSwiftPackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftPackageApp.swift
+//  testSwiftPackage
+//
+//  Created by Viet Anh Dang on 02/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftPackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
