1bdedb9b254a3fd8e7648421f49275282e10c07c
Viet Anh Dang <vietanh.dang282@gmail.com>
2022-06-02 11:18:44 +0700

    Add Swift UI View

--- MyLibrary/Sources/MyLibrary/MyLibrary.swift
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -2,5 +2,6 @@ public struct MyLibrary {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        
     }
 }
---
+++ MyLibrary/Sources/SwiftUIView.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Viet Anh Dang on 02/06/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello from SPM")
+    }
+}
